7f6add3abae6c3ed7d73a77baebdbf2c2949c3d4
Emanuel Landeholm <emanuel.landeholm@gmail.com>
2012-02-28 08:26:45 +0100

    Playing around with the unit test framework.
    Signed-off-by: Emanuel Landeholm <emanuel.landeholm@gmail.com>

--- bootstrapper.php
+++ bootstrapper.php
@@ -1,11 +1,17 @@
 <?php namespace ovasen;
 
 use \ovasen\core\ClassLoader;
-use \ovasen\test\TheTestClass;
+use \ovasen\core\test\TheTestClass;
 
 define("ROOT_PATH", dirname(__FILE__) );
 define("NAME_SPACE", "ovasen");
 require "core" . DIRECTORY_SEPARATOR . "class_loader.php";
 $cl = new ClassLoader(ROOT_PATH, NAME_SPACE);
-$my_class = new TheTestClasz();
+$my_class = new TheTestClass();
 echo "file_name is " . $cl->fqClassNameToFileName(get_class($my_class)) . PHP_EOL;
+
+// simplistic unit testing
+
+$testcase = new \ovasen\core\test\ClassLoader();
+$report = $testcase->doTests();
+print_r($report);
