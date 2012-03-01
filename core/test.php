9eb4b346972b22539fa6589b232b05c4152c6334
Emanuel Landeholm <emanuel.landeholm@gmail.com>
2012-03-01 01:52:24 +0100

    Added some Tester/Testee infrastructure. Modified Test to look for Tester's Testee in same dir, but sans the "Tester" class name suffix. Added "all_passed" member with obvious semantics. Info from backtrace is now correct. Line refers to Tester class. Refactored asserts a bit, added assertNotEquals and assertStringEqual. Singleton is now an abstract class, so now using class_parents instead of class_implements.
    Signed-off-by: Emanuel Landeholm <emanuel.landeholm@gmail.com>

--- core/test.php
+++ core/test.php
@@ -1,74 +1,156 @@
 <?php namespace ovasen\core;
 
-use \ovasen\core\TestException;
+// Really trivial unit testing protocol
+// 
+// OK, suppose you wanted to unit test "my_class.php" defining a class named
+// MyClass. This is your Testee
+// 
+// o Create a Tester class named "my_class_tester.php" defining a class
+//   name MyClassTester.
+// o Tester class extends Test, nothing else
+// o Add methods that test specific aspects of Testee. The base class Test
+//   automatically gives you an instance of Testee in $this->instance
+// o Make sure your Tester method are descriptive and that the names begin with
+//   "test", all other methods are ignored. This is useful for utility methods in
+//    the Tester
+// o Use the $this->assert() & $this->assertEquals() methods inherited from Test
+//   to pass invariants back to the Test base class
+//
+// To run the tests simply:
+//   
+// o Instatiate the tester, ie.
+//   $tester_instance = new MyClassTester();
+// o do:
+//   $report = $tester_instance->doTests()
+// o This method is defined in the Test base class and it returns a simple
+//   array based report of all the tests discovered and ran
+// o Once you have called $tester_instance->doTests(), you may query
+//   $tester_instance->passed(), which returns true iff all tests passed
 
 class Test
 {
-    private $test_class_name;
-    private $test_methods;
-    private $class_name;
-    protected $instance;
+    private $all_passed;
+    private $tester_class_name;
+    private $tester_methods;
+    private $testee_class_name; // testee class name derived from tester ditto
+    protected $instance;        // instance of testee
+    const TESTER_CLASS_SUFFIX = "Tester";
+    const TESTER_METHOD_PREFIX = "test";
+    
+    private static function eatSuffix($str, $suffix) {
+        $len = strlen($suffix);
+        if (substr($str, -$len) === $suffix) {
+            return substr($str, 0, strlen($str) - $len);
+        }
+        return false;        
+    }
+    
+    private static function prefixMatch($str, $prefix) {
+        return substr($str, 0, strlen($prefix)) === $prefix;
+    }
+    
+    private function setClassNames() {
+        $this->tester_class_name = get_class($this);
+        $sans_suffix = self::eatSuffix($this->tester_class_name,
+            self::TESTER_CLASS_SUFFIX);
+        if ($sans_suffix === false) {
+            trigger_error(sprintf("Tester class name \"%s\" does not end in Tester",
+                $this->tesster_class_name));
+        }
+        $this->testee_class_name = $sans_suffix;      
+    }
     
     public function __construct() {
-        $this->test_class_name = get_class($this);
+        $this->setClassNames();
+        // Singleton testees need special treatment!
+        // These classes are not meant to be instantiated
+        // from the outside, they will crash and burn if you try.
+        // This is basically an example of the "marker interface"
+        // design pattern
         
-        $parts = explode("\\", $this->test_class_name);
-        $class_name = array_pop($parts);
-        @array_pop($parts); // gets rid of "test" subdir
-        $parts[] = $class_name;
-        $this->class_name = implode("\\", $parts);
-        $implements = class_implements($this->class_name);
-        
-        // we use Singleton as a marker interface here
-        
-        if (isset($implements["ovasen\\core\\Singleton"])) {
-            $this->instance = call_user_func(array($this->class_name, "getInstance"));
+        $bases = class_parents($this->testee_class_name);
+        if (isset($bases["ovasen\\core\\Singleton"])) {
+            $this->instance = call_user_func(array($this->testee_class_name, "getInstance"));
         }
         else {
-            $this->instance = new $this->class_name;
+            $this->instance = new $this->testee_class_name;
         }
         $this->registerTests();
     }
     
-    protected function assert($condition) {
-        if ($condition !== true) {
-            throw new TestException("assert: condition: $condition");
+    private static function failAssert($type, $description=null) {
+        // we return the exception and throw it in assert*(), otherwise we would
+        // get another stack frame in the trace which is bother
+       
+        if ($description !== null) {
+            return new TestException(sprintf("%s: failed (%s)", $type, $description));
+        }
+        else {
+            return new TestException(sprintf("%s: failed", $type));
         }
     }
     
-    protected function assertEquals($a, $b) {
-        if ($a !== $b) {
-            throw new TestException("assertEquals");
+    protected function assert($condition, $description = null) {
+        if ($condition !== true) {
+            throw self::failAssert(__METHOD__, $description);
         }
     }
+    
+    protected function assertEquals($a, $b, $description = null) {
+        if ($a !== $b) {
+            throw self::failAssert(__METHOD__, $description);
+        }
+    }
+    
+    protected function assertNotEquals($a, $b, $description = null) {
+        if ($a === $b) {
+            throw self::failAssert(__METHOD__, $description);
+        }
+    }
+    
+    protected function assertStringEqual($a, $b, $description = null) {
+        if (!is_string($a) || !is_string($b) || strcmp($a, $b)) {
+            throw self::failAssert(__METHOD__, $description);
+        }
+    }
+    
+    // Register all the test methods in the testee
     
     protected function registerTests() {
-        $this->test_methods = array();
-        $methods = get_class_methods($this->test_class_name);
+        $this->tester_methods = array();
+        $methods = get_class_methods($this->tester_class_name);
         foreach ($methods as $method) {
-            if (substr($method, 0, 4) === "test") {
-                $this->test_methods[] = $method;
+            if (self::prefixMatch($method, "test")) {
+                $this->tester_methods[] = $method;
             }
         }
     }
     
     public function doTests($stop_on_error = true) {
+        $this->all_passed = true;
         $report = array();
         $stop = false;
-        foreach ($this->test_methods as $method) {
+        foreach ($this->tester_methods as $method) {
+            echo "$method" . PHP_EOL;
+            $te = null;
             try {
                 $this->$method();
             }
             catch (TestException $te) {
+                $this->all_passed = false;
                 $stop = $stop_on_error;
                 $traces = $te->getTrace();
-                $trace = $traces[0];
-                $report[$method]["class"] = $this->class_name;
-                $report[$method]["test"] = $this->test_class_name;
+                $trace = $traces[1]; // $traces[0] is us, [1] is the client
+                $report[$method]["testee"] = $this->testee_class_name;
+                $report[$method]["tester"] = $this->tester_class_name;
                 $report[$method]["message"] = $te->getMessage();
                 $report[$method]["file"] = $trace["file"];
                 $report[$method]["line"] = $trace["line"];
+//                $report[$method]["snippet"] = self::getSnippet($trace["file"], $trace["line"]);
                 $report[$method]["xyzzy"] = get_class($this->instance);
+            }
+            if (!($te instanceof TestException)) {
+                $report[$method] = "passed";
             }
             if ($stop) {
                 break;
