c837c83d46f198d48030e5a4558f3626610e400d
Emanuel Landeholm <emanuel.landeholm@gmail.com>
2012-03-01 13:42:06 +0100

    constructor now triggers error if called more than once, clients that override __construct() must call parent::__construct().
    Signed-off-by: Emanuel Landeholm <emanuel.landeholm@gmail.com>

--- core/singleton.php
+++ core/singleton.php
@@ -5,7 +5,10 @@ abstract class Singleton {
 
     // protected: you may override me but you cannot call me from the outside!
     protected function __construct() {
-        ;
+        if (isset(self::$instance)) {
+            trigger_error("There can be only one " . get_class($this));
+        }
+        self::$instance = $this;
     }
 
     public static function getInstance($args = null) {
