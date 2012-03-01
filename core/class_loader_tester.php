83acd24f17175c3d4dec2d13eede0fa41c8d90be
Emanuel Landeholm <emanuel.landeholm@gmail.com>
2012-03-01 13:42:50 +0100

    Tester for ClassLoader.
    Signed-off-by: Emanuel Landeholm <emanuel.landeholm@gmail.com>

---
+++ core/class_loader_tester.php
@@ -0,0 +1,30 @@
+<?php namespace ovasen\core;
+
+use ovasen\core\Test;
+use ovasen\core\ClassLoader as CL; // or name conflict ensues
+
+// Silly example of unit testing
+// If the ClassLoader was broken enough to not pass
+// the tests given below, we would never get here in
+// the first place. :-/
+
+class ClassLoaderTester extends Test
+{
+    public function testFileNameToFqClassName() {
+        $file_name = "core" . DIRECTORY_SEPARATOR . "singleton.php";
+        $class_name = $this->instance->fileNameToFqClassName($file_name);
+        $reference = "ovasen" . CL::NAMESPACE_DELIMITER .
+            "core" . CL::NAMESPACE_DELIMITER . "Singleton";
+        
+        $this->assert(!strcmp($class_name, $reference));
+    }
+    public function testYesYesYes() {
+        $this->assertEquals(42, 42);
+    }
+    public function testImpossible() {
+        $this->assertEquals(17, 42);
+    }
+    public function testNotRan() {
+        $this->assert(false);
+    }
+}
