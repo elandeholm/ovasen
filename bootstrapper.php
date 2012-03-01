4398d5f120fbf2bcb7a7f5959ef92de0e53c313a
Emanuel Landeholm <emanuel.landeholm@gmail.com>
2012-03-01 14:45:30 +0100

    Added a minimal error handler to make sure PHP stops on E_NOTICE and doesn't flood my cmd window with page after page of useless crap.
    Signed-off-by: Emanuel Landeholm <emanuel.landeholm@gmail.com>

--- bootstrapper.php
+++ bootstrapper.php
@@ -1,5 +1,15 @@
 <?php namespace ovasen;
 
+error_reporting(E_ALL | E_STRICT);
+ini_set('display_error', TRUE);
+
+function myErrorHandler($errno, $errstr, $errfile, $errline) {
+    echo "$errstr $errfile:$errline" . PHP_EOL;
+    die();
+}
+
+set_error_handler('ovasen\myErrorHandler');
+
 use \ovasen\core\ClassLoader;
 use \ovasen\core\test\TheTestClass;
 use \ovasen\core\ClassLoaderTester;
@@ -7,9 +17,6 @@ use \ovasen\core\ClassLoaderTester;
 define("ROOT_PATH", dirname( __FILE__ ));
 define("NAME_SPACE", "ovasen");
 require "core" . DIRECTORY_SEPARATOR . "class_loader.php";
-$my_class = new TheTestClass();
-echo "file_name is " . ClassLoader::getInstance()
-    ->fqClassNameToFileName(get_class($my_class)) . PHP_EOL;
 
 // simplistic unit testing
 
